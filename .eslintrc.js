0fbbcd8f4c2f0059631eed7ecd909e7f141ccef2
matsushinDB11 <matsushin35@gmail.com>
2023-01-17 02:55:00 +0900

    :package: install eslint

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.eslint.json',
+    tsconfigRootDir: __dirname,
+  },
+  ignorePatterns: ['dist', '*.d.ts', '*.js'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+};
