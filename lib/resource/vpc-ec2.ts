9325d11ec9e405498075b2f9610be4820d14790d
matsushinDB11 <matsushin35@gmail.com>
2023-01-22 18:15:55 +0900

    :sparkles: create seciruty group

--- lib/resource/vpc-ec2.ts
+++ lib/resource/vpc-ec2.ts
@@ -8,6 +8,9 @@ import {
   InstanceSize,
   InstanceType,
   IpAddresses,
+  Peer,
+  Port,
+  SecurityGroup,
   SubnetType,
   Vpc,
 } from 'aws-cdk-lib/aws-ec2';
@@ -17,18 +20,28 @@ export class VpcEc2 extends Construct {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
-    const vpc = new Vpc(scope, 'ArkVpc', {
+    const vpc = new Vpc(this, 'ArkVpc', {
       ipAddresses: IpAddresses.cidr('10.0.0.0/16'),
       vpcName: 'ark-vpc',
     });
 
-    const keyPair = new CfnKeyPair(scope, 'ArkKeyPair', {
+    const keyPair = new CfnKeyPair(this, 'ArkKeyPair', {
       keyName: 'ark-key',
       keyType: 'ED25519',
     });
     keyPair.applyRemovalPolicy(RemovalPolicy.DESTROY);
 
-    const ec2 = new Instance(scope, 'ArkEc2', {
+    const ec2SecurityGroup = new SecurityGroup(this, 'ark-ec2-security-group', {
+      vpc: vpc,
+      allowAllOutbound: true,
+    });
+    ec2SecurityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(22), 'ssh');
+    ec2SecurityGroup.addIngressRule(Peer.anyIpv4(), Port.udp(27015), 'Steam server browser query');
+    ec2SecurityGroup.addIngressRule(Peer.anyIpv4(), Port.udp(7777), 'Game client');
+    ec2SecurityGroup.addIngressRule(Peer.anyIpv4(), Port.udp(7778), 'UDP Socket');
+    ec2SecurityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(27020), 'RCON');
+
+    const ec2 = new Instance(this, 'ArkEc2', {
       vpc: vpc,
       instanceType: InstanceType.of(InstanceClass.T3, InstanceSize.MEDIUM),
       machineImage: new AmazonLinuxImage({
@@ -36,6 +49,7 @@ export class VpcEc2 extends Construct {
       }),
       keyName: Token.asString(keyPair.ref),
       vpcSubnets: vpc.selectSubnets({ subnetType: SubnetType.PUBLIC }),
+      securityGroup: ec2SecurityGroup,
     });
   }
 }
